071a67ea999b100b9ab019cf2394737050540e68
oomuro <youkosama@naver.com>
2018-05-18 03:32:55 +0900

    stop n wait fix

--- stop_n_wait/receiver.jl
+++ stop_n_wait/receiver.jl
@@ -1,23 +1,31 @@
 @async begin
-    server = listen(2000)
+    data = 0
     expected_num = 1
-    conn = accept(server)
+    rng = MersenneTwister(1234)
+    # server = listen(2000)
+    sock = UDPSocket()
+    bind(sock, ip"127.0.0.1", 1000)
+    # conn = accept(server)
     while true
-        while true
-            trial = 1
-            data = read(conn, Int64)
-            sleep(1.0)
-            if data == expected_num
-                println("\nData Received... : $data")
-                sleep(1.0)
-                break
-            else
-                println("No Data Received... Trying Again : $trial")
-                trial += 1
-            end
+        sleep(1.0)
+        trial = 1
+        # data = read(conn, Int64)
+        @async data = parse(Int64, String(recv(sock)))
+        # sleep(1.0)
+        if data == expected_num
+            println("\nData Received... : $data")
+            sleep(2*rand!(rng, zeros(5))[rand(1:5)])
+            send(sock, ip"127.0.0.1", 2000, "$expected_num")
+            println("Sending ACK... : $expected_num")
+            expected_num += 1
+            # break
+        # else
+            # println("No Data Received... Trying Again : $trial")
+            # trial += 1
         end
-        write(conn, expected_num)
-        println("Sending ACK... : $expected_num")
-        expected_num += 1
+        # write(conn, expected_num)
+        # send(sock, ip"127.0.0.1", 2000, "$expected_num")
+        # println("Sending ACK... : $expected_num")
+        # expected_num += 1
     end
 end
--- stop_n_wait/sender.jl
+++ stop_n_wait/sender.jl
@@ -1,25 +1,33 @@
-seq = 1
 @async begin
     seq = 1
-    line = 1
-    conn = connect(2000)
-    sleep(1.0)
-    for i in 1:20
-        trial = 1
-        write(conn, seq)
+    line = 0
+    trial = 1
+    # conn = connect(2000)
+    sock = UDPSocket()
+    bind(sock, ip"127.0.0.1", 2000)
+    while true
+        sleep(1.0)
+        # write(conn, seq)
+        send(sock, ip"127.0.0.1", 1000, "$seq")
         println("\nSending Data... : $seq")
-        while true
-            @async line = read(conn, Int64)
+        # while true
+        # @async line = read(conn, Int64)
+        @async line = parse(Int64, String(recv(sock)))
+        # sleep(1.0)
+        if line == seq
+            println("ACK Received... : $line")
             sleep(1.0)
-            if line == seq
-                println("ACK Received... : $line")
-                sleep(1.0)
-                seq += 1
-                break
-            else
-                println("Failed receiving ACK... Trying Again : $trial")
-                trial += 1
-            end
+            seq += 1
+            trial = 1
+            # break
+        else
+            println("Failed Receiving ACK... Retry : $trial")
+            sleep(1.0)
+            # println("Failed receiving ACK... Trying Again : $trial")
+            # println("Wrong ACK... : $line")
+            trial += 1
         end
+            # break
+        # end
     end
 end
